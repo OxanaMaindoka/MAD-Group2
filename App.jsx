c213259d8e5a120d3a4d7083c8b11ea730d3a78f
Clement7-bit <s2200635@student.unklab.ac.id>
2024-02-22 10:53:20 +0800

    Update

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, ScrollView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Register, UserList} from './screens';
+import {Register, UserList} from './screen';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
