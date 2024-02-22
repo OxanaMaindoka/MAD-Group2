e052b547879abc8458cb6c4e428b5cb1ad219e02
watur <waturandjordy@gmail.com>
2024-02-22 11:58:35 +0800

    Fix UserList

---
+++ screen/UserList.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import axios from '../node_modules/axios/index';
+
+export default function UserList() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(data => {
+            setData(data.data)
+        
+        }).catch(err => console.log(err))
+    })
+  return (
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <View style={styles.utama}>
+          <Text style={styles.title}>Users List</Text>
+          {data?.map(user => (
+            <View key={user.id} style={styles.userContainer}>
+              <Text style={styles.boldText}>Name: <Text style={styles.normalText}>{user.name}</Text></Text>
+              <Text style={styles.boldText}>Username: <Text style={styles.normalText}>{user.username}</Text></Text>
+              <Text style={styles.boldText}>Email: <Text style={styles.normalText}>{user.email}</Text></Text>
+              <Text style={styles.boldText}>Address: <Text style={styles.normalText}>{user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</Text></Text>
+              <Text style={styles.boldText}>Phone: <Text style={styles.normalText}>{user.phone}</Text></Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
